e9fed9cc69f4296f95822a9d2e7cf244da78071e
daiginzyo <daiginzyo>
2020-12-10 01:42:24 +0900

    add nfc_permission

--- lib/repositories/nfc_result_repository.dart
+++ lib/repositories/nfc_result_repository.dart
@@ -3,11 +3,5 @@ import 'package:state_notifier/state_notifier.dart';
 class NfcResultRepository extends StateNotifier<String> {
   NfcResultRepository() : super('');
 
-  void exportResult(readResult) {
-    if (readResult == null) {
-      state = 'read again';
-    } else {
-      state = readResult;
-    }
-  }
+  void exportResult(readResult) => state = readResult;
 }
